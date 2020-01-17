59d73eba2de1b01d38828cf339fc193d09d2a0bb
Eian Carter <eian@bricleir.com>
2020-01-17 08:04:35 -0800

    converted app.js to class  component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayerCard from './components/PlayerCard';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+    console.log('constructor works!')
+  }
+
+  componentDidMount(){
+
+    axios
+    .get(`http://localhost:5000/api/players`)
+    .then(response => {
+      console.log(response)
+      this.setState({
+        followers: response.data
+      });
+    })
+    .catch( error => {
+      console.log('try again!', error);
+    })
+  }
+
+  componentWillUnmount() {
+  }
+
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('update is woking!', prevState.cards, this.state.cards);
+    console.log('followers is working!', this.state.followers);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PlayerCard players={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
